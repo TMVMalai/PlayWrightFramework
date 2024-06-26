3d810a24cc4b37811bbce22bd99bf8629afd1caa
Thirumalaivasan Perumal <154405234+TMVMalai@users.noreply.github.com>
2024-06-26 19:41:02 +0530

    Update PlaywrightInitialization.java

--- src/test/java/testCases/PlaywrightInitialization.java
+++ src/test/java/testCases/PlaywrightInitialization.java
@@ -1,7 +1,8 @@
 package testCases;
 
+import org.playwright.dataproviderclass.DataProviderClasses;
 import org.testng.annotations.Test;
-import java.util.HashMap;
+
 import java.util.Map;
 
 import org.playwright.modules.Modules;
@@ -10,13 +11,13 @@ import baseFunctions.CommonFunctions;
 
 public class PlaywrightInitialization extends CommonFunctions {
 
-	@Test(enabled = true, groups = "Regression")
-	public void searchVedioFunctionaly() {
-		Map<String, Object> testData = new HashMap<String, Object>();
-		Modules m = new Modules(testData, page);
-		System.out.println("Login Completed");
-		//m.testClickFunctionality();
-	}
-	
+    @Test(enabled = true, groups = "Regression", dataProviderClass = DataProviderClasses.class, dataProvider = "getJsonData")
+    public void signupFunctionality(Object data) {
+        Map<String, Object> testData = (Map<String, Object>) data;
+        Modules m = new Modules(testData, page);
+        System.out.println("Perform Login");
+        m.testClickFunctionality();
+    }
+
 
 }
