7ab306d38789cd98797eef3b49ca51ec7c246ad0
Thirumalaivasan Perumal <154405234+TMVMalai@users.noreply.github.com>
2024-06-26 19:38:30 +0530

    Update DemoPage.java

--- src/main/java/org/playwright/pages/DemoPage.java
+++ src/main/java/org/playwright/pages/DemoPage.java
@@ -1,42 +1,66 @@
 package org.playwright.pages;
 
-import java.util.List;
+public class DemoPage extends BaseClass {
 
-import org.framework.playwright.annotations.FindBy;
-import org.framework.playwright.utils.UtilityClass;
+    public DemoPage(Page page) {
+        super(page);
+    }
 
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
+    @FindBy(xpath = "//a[text()='Joe.Root']//parent::td/..//following-sibling::td//input[@type='checkbox']")
+    private Locator element;
 
-public class DemoPage extends UtilityClass {
+    @FindBy(xpath = "//div[text()='Login']")
+    private Locator loginButton;
 
-	public Page page;
+    @FindBy(xpath = "//a[text()='Username']//ancestor::thead/following-sibling::tbody//tr//td[2]")
+    private Locator userNames;
 
-	public DemoPage(Page page) {
-		this.page = page;
-		initElements(page, this);
-	}
+    @FindBy(xpath = "//input[@data-testid='user-mobileno-input-box']")
+    private Locator emailID;
 
-	@FindBy(xpath = "//a[text()='Joe.Root']//parent::td/..//following-sibling::td//input[@type='checkbox']")
-	private Locator element;
+    @FindBy(xpath = "//input[@data-testid='password-input-box-cta']")
+    private Locator password;
 
-	@FindBy(xpath = "//a[text()='Username']//ancestor::thead/following-sibling::tbody//tr//td[2]")
-	private Locator userNames;
+    @FindBy(xpath = "(//div[text()='Email']//parent::div/../div[contains(@class,'css-1dbjc4n r-zso239') or (@class='css-1dbjc4n')])[1]")
+    private Locator emailRadioButton;
 
-	public void clickCheckBox() {
-		element.click();
-	}
+    @FindBy(xpath = "//div[@data-testid='login-cta']")
+    private Locator signIn;
 
-	public List<String> getUserNames() {
-		List<String> userNamesList = userNames.allTextContents();
-		return userNamesList;
-	}
 
-	public void clickAllUserNames() {
-		List<Locator> elements = getLocators(userNames);
-		for (Locator ele : elements) {
-			ele.click();
-		}
-	}
+    public void clickCheckBox() {
+        click(element, "checkBox");
+    }
 
+    public List<String> getUserNames() {
+        List<String> userNamesList = userNames.allTextContents();
+        return userNamesList;
+    }
+
+    public void clickAllUserNames() {
+        List<Locator> elements = getLocators(userNames);
+        for (Locator ele : elements) {
+            ele.click();
+        }
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public void clickEmailRadioButton() {
+        emailRadioButton.click();
+    }
+
+    public void sendEmail(String email) {
+        emailID.fill(email);
+    }
+
+    public void sendpassword(String Password) {
+        password.fill(Password);
+    }
+
+    public void clickSignInButton() {
+        signIn.click();
+    }
 }
